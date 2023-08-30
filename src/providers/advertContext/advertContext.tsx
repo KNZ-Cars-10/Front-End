063762150b45fe4ce1aa17463ed083da8dfc786d
GustavoCruz <gustavo0201cruz@gmail.com>
2023-08-30 13:26:21 -0400

    fix: add modal create button

--- src/providers/advertContext/advertContext.tsx
+++ src/providers/advertContext/advertContext.tsx
@@ -45,7 +45,6 @@ export const AdvertProvider = ({ children }: IDefaultProviderProps) => {
   const [externalBrand, setExternalBrand] = useState<string | null>(null);
   const [currentCar, setCurrentCar] = useState<any | []>([]);
   const [externalModel, setExternalModel] = useState<string | null>(null);
-
   const [brand, setBrand] = useState<string | null>(null);
   const [model, setModel] = useState<string | null>(null);
   const [color, setColor] = useState<string | null>(null);
